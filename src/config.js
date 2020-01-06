a5400215556df44e7f1cf1f180676c208e19c817
Nicholas Gibson <Nwgibson85@gmail.com>
2020-01-06 02:53:44 -0600

    updated DB_URL to DATABASE_URL for deployment

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_KEY: process.env.API_KEY,
-    DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/noteful_folders',
-    TEST_DB_URL: process.env.TEST_DB_URL
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful_folders',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
   }
