150d9c4acf98a87baf31dd50fcbc6cb6e1a53856
Nicholas Gibson <Nwgibson85@gmail.com>
2020-01-15 16:42:16 -0600

    deployed successfully

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_KEY: process.env.API_KEY,
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
+    DATABASE_URL: process.env.HEROKU_POSTGRESQL_PURPLE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
+    TEST_DATABASE_URL: process.env.TEST_HEROKU_POSTGRESQL_PURPLE_URL
   }
